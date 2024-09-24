e5af61b6437ef5d02d183eb899e702109005bbcc
ranjithmanju <ranjithk30694@gmail.com>
2024-09-24 22:28:41 +0530

    pushing first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className='text-red-500'>hello</h1>
+     <h1 className='text-red-500'>hello world</h1>
     </div>
   );
 }
