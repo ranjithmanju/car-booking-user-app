b6c474a76d77463bdd2966d5c319374193f6cc7a
ranjithmanju <ranjithk30694@gmail.com>
2024-09-25 12:03:49 +0530

    create login and signup page in root directory app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './routes/Login';
+import Signup from './routes/Signup';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='text-red-500'>hello world</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
